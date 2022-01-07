fcea79cc7f508fba386af8d288101d559fc78520
julien-gcvs <julien.gcvs@gmail.com>
2022-01-07 13:52:37 +0100

    +

---
+++ replace.php
@@ -0,0 +1,86 @@
+<?php
+$folderPath = './area-58Pack/assets/minecraft/optifine/optifine';
+
+if (!function_exists('str_starts_with')) {
+    function str_starts_with($haystack, $needle) {
+        return (string)$needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0;
+    }
+}
+if (!function_exists('str_ends_with')) {
+    function str_ends_with($haystack, $needle) {
+        return $needle !== '' && substr($haystack, -strlen($needle)) === (string)$needle;
+    }
+}
+if (!function_exists('str_contains')) {
+    function str_contains($haystack, $needle) {
+        return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+    }
+}
+
+function getDirContents($dir, &$results = []) {
+    $files = scandir($dir);
+
+    foreach ($files as $key => $value) {
+        $path = realpath($dir . DIRECTORY_SEPARATOR . $value);
+        if (!is_dir($path))
+        {
+            $results[] = $path;
+        }
+        else if ($value != "." && $value != "..")
+        {
+            getDirContents($path, $results);
+            $results[] = $path;
+        }
+    }
+
+    return $results;
+}
+
+$count = 0;
+$paths = getDirContents($folderPath);
+$names = [];
+foreach ($paths as $k => $path)
+{
+    if (!is_dir($path) && str_ends_with($path, '.properties'))
+    {
+        $pathname = end(explode('/',$path));
+        // read the file
+        $file = file_get_contents($path);
+        // replace the data
+        $lines = explode("\n", $file);
+        $file2 = '';
+        foreach ($lines as $line)
+        {
+            if (str_starts_with('nbt.display.Name'))
+            {
+                $line2 = str_replace('nbt.display.Name=','',$line);
+                if (str_starts_with('iregex:('))
+                {
+                    $line2 = str_replace('iregex:(','',$line2);
+                    $line2 = str_replace(')','',$line2);
+                }
+                $line2 = explode('|', $line2);
+                $line2 = $line2[0];
+            }
+            $line = 'nbt.nbrData.skinName' .
+            $line = str_replace('iregex:(','',$line);
+            $line = str_replace(')','',$line);
+            $line = str_replace(' ','_',$line);
+
+            $file2.=$line;
+        }
+
+
+
+        if (!$file2 && $file)
+        {
+            echo "erreur: $path\n";
+        }
+
+        // write the file
+        if ($file2)
+        {
+            file_put_contents($path,$file2);
+        }
+    }
+}
